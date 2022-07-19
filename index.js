65d0822f7a108f9c86076dd0eda66e6775ca8848
fanlia <you@example.com>
2022-07-19 19:30:05 +0800

    refine

--- index.js
+++ index.js
@@ -3,31 +3,41 @@ import xlsx from '@ailnaf/xlsx2csv'
 import { stringify } from 'csv-stringify'
 import { Readable, Transform } from 'stream'
 
+class XLSXReadable extends Readable {
+
+    constructor(file, options = {}) {
+        super({
+            objectMode: true,
+        })
+
+        const { json = false } = options
+
+        let header = undefined
+
+        xlsx(file, data => {
+            if (json) {
+                if (header) {
+                    this.push(header.reduce((m, d, i) => ({...m, [d]: data[i]}), {}))
+                } else {
+                    header = data
+                }
+            } else {
+                this.push(data)
+            }
+        })
+        .then(() => {
+            this.push(null)
+        })
+        .catch((e) => this.destroy(e))
+    }
+
+    _read(size) {
+    }
+}
+
 export default function xlsx2stream(file, options = {}) {
 
-    const { json = false } = options
-
-    let header = undefined
-
-    return new Readable({
-        objectMode: true,
-        read() {
-            xlsx(file, data => {
-                if (json) {
-                    if (header) {
-                        this.push(header.reduce((m, d, i) => ({...m, [d]: data[i]}), {}))
-                    } else {
-                        header = data
-                    }
-                } else {
-                    this.push(data)
-                }
-            })
-            .then(() => this.push(null))
-            .catch((e) => this.destroy(e))
-        },
-    })
-
+    return new XLSXReadable(file, options)
 }
 
 export function xlsx2csv(file) {
