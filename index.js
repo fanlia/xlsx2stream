02db577461e4aa82c834c7a825752cabb4f574cc
fanlia <you@example.com>
2022-07-20 10:18:07 +0800

    refine

--- index.js
+++ index.js
@@ -3,41 +3,40 @@ import xlsx from '@ailnaf/xlsx2csv'
 import { stringify } from 'csv-stringify'
 import { Readable, Transform } from 'stream'
 
-class XLSXReadable extends Readable {
+export default function xlsx2stream(file, options = {}) {
 
-    constructor(file, options = {}) {
-        super({
-            objectMode: true,
-        })
+    const { json = false } = options
 
-        const { json = false } = options
+    let started = false
 
+    const start = (reader) => {
+        if (started) return
+        started = true
         let header = undefined
 
         xlsx(file, data => {
             if (json) {
                 if (header) {
-                    this.push(header.reduce((m, d, i) => ({...m, [d]: data[i]}), {}))
+                    reader.push(header.reduce((m, d, i) => ({...m, [d]: data[i]}), {}))
                 } else {
                     header = data
                 }
             } else {
-                this.push(data)
+                reader.push(data)
             }
         })
         .then(() => {
-            this.push(null)
+            reader.push(null)
         })
-        .catch((e) => this.destroy(e))
+        .catch((e) => reader.destroy(e))
     }
 
-    _read(size) {
-    }
-}
-
-export default function xlsx2stream(file, options = {}) {
-
-    return new XLSXReadable(file, options)
+    return new Readable({
+        objectMode: true,
+        read() {
+            start(this)
+        },
+    })
 }
 
 export function xlsx2csv(file) {
